b1fae93da826d3207c71338c4245ba183819a9e1
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-11-25 18:24:10 +0530

    dummy function added

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,1 +1,12 @@
 // GET logged-in user profile
+// add a dummy get function to prevent build error
+
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({
+        success: true,
+        message: 'User profile fetched successfully',
+        data: {}
+    }, { status: 200 });
+}

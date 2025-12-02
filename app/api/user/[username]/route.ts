d367c3d788530bdfaf71db92c16f3fb72e40c35a
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-12-02 18:02:00 +0530

    speeling

--- app/api/user/[username]/route.ts
+++ app/api/user/[username]/route.ts
@@ -14,7 +14,7 @@ export async function GET(request: NextRequest, { params }: { params: { username
 
         if (!user) {
             return NextResponse.json({
-                sucess: false,
+                success: false,
                 message: "User not found"
             }, {
                 status: 404
